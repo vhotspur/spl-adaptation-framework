91a88f87f279beee4524f9c58f9706d0386e5fcc
Vojtech Horky <vojtech.horky@gmail.com>
2012-09-06 13:10:08 +0200

    Print more statistics on exit

--- src-example/cz/cuni/mff/d3s/spl/example/newton/checker/SlaChecker.java
+++ src-example/cz/cuni/mff/d3s/spl/example/newton/checker/SlaChecker.java
@@ -1,6 +1,7 @@
 package cz.cuni.mff.d3s.spl.example.newton.checker;
 
 import cz.cuni.mff.d3s.spl.core.data.SerieDataSource;
+import cz.cuni.mff.d3s.spl.core.data.Statistics;
 import cz.cuni.mff.d3s.spl.core.data.instrumentation.InstrumentingDataSource;
 import cz.cuni.mff.d3s.spl.core.formula.Formula;
 import cz.cuni.mff.d3s.spl.core.formula.Result;
@@ -9,7 +10,6 @@ import cz.cuni.mff.d3s.spl.core.formula.SlaFormula;
 public class SlaChecker implements Runnable {
 
 	private final static String METHOD = "org.apache.commons.math.analysis.solvers.NewtonSolver#solve";
-			//"org.apache.commons.math.analysis.polynomials.PolynomialFunction#value";
 	private final static long SEC_TO_NANOS = 1000 * 1000 * 1000;
 
 	SerieDataSource source;
@@ -23,7 +23,14 @@ public class SlaChecker implements Runnable {
 	@Override
 	public void run() {
 		Result result = sla.evaluate();
+		Statistics stats = source.get();
+
+		/* Explicitly flush all previous output to prevent interleaving. */
+		System.out.flush();
+		System.err.flush();
 		
-		System.err.printf("Contract of NewtonSolver.solve(): %s.\n", result);
+		System.err.printf(
+				"NewtonSolver.solve(): mean is %2.1fns, %d samples: %s.\n",
+				stats.getArithmeticMean(), stats.getSampleCount(), result);
 	}
 }
