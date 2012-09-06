82e40ddbbec0fd6da66fa6957a5443508965dc6a
Vojtech Horky <vojtech.horky@gmail.com>
2012-09-06 10:18:28 +0200

    Use implicitly synchronized map

--- src/cz/cuni/mff/d3s/spl/core/data/storage/InMemorySamples.java
+++ src/cz/cuni/mff/d3s/spl/core/data/storage/InMemorySamples.java
@@ -1,33 +1,41 @@
 package cz.cuni.mff.d3s.spl.core.data.storage;
 
+import java.util.Collections;
 import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 import cz.cuni.mff.d3s.spl.core.data.SampleStorage;
 
 /** Stores measured samples in memory. */
 public class InMemorySamples implements SampleStorage {
 
-	private SortedMap<Long, Long> data = new TreeMap<>();
+	private SortedMap<Long, Long> data = new ConcurrentSkipListMap<>();
 	private String id = "";
+	private int hash = 0;
 	
 	public InMemorySamples(String id) {
 		this.id = id;
+		this.hash = id.hashCode();
 	}
 
 	@Override
-	public synchronized void add(long sample, long clock) {
+	public void add(long sample, long clock) {
 		data.put(clock, sample);
 	}
 
 	@Override
-	public synchronized SortedMap<Long, Long> get(long startTime, long endTime) {
-		return new TreeMap<>(data.subMap(startTime, endTime));
+	public SortedMap<Long, Long> get(long startTime, long endTime) {
+		return Collections.unmodifiableSortedMap(data.subMap(startTime, endTime));
 	}
 	
 	@Override
 	public String toString() {
 		return String.format("Samples[%s](%d)", id, data.size());
+	}
+	
+	@Override
+	public int hashCode() {
+		return hash;
 	}
 
 	@Override
