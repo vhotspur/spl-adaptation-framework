93881f40311d8a0d7832e07c211d9b6ddc2987b7
Vojtech Horky <vojtech.horky@gmail.com>
2012-09-19 11:16:27 +0200

    Add script to compare performance of the agent

---
+++ compare-newton.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+LOOPS="$1"
+shift
+
+[ -z "$LOOPS" ] && exit 1
+
+run_measure() {
+	echo "$1"
+	shift
+	./measure-newton.sh "$LOOPS" "$@" 2>&1 | tail -n 1 | sed 's/^[>]*//'
+}
+
+run_measure "Without any agent"
+run_measure "With agent, without measuring" --std-agent --no-meter
+run_measure "With agent, with measuring" --std-agent
