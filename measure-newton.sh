a8fff20bf9158295c7df3d36bc3254fef9271d6a
Vojtech Horky <vojtech.horky@gmail.com>
2012-09-24 10:34:32 +0200

    Improve statistics report in Newton measuring

--- measure-newton.sh
+++ measure-newton.sh
@@ -5,10 +5,18 @@ shift
 
 [ -z "$LOOPS" ] && exit 1
 
-if [ "$1" = "--std-agent" ]; then
-	EXTRA_OPTS="-javaagent:out/jar/spl-agent.jar=spl.shutdown.class=cz.cuni.mff.d3s.spl.example.newton.checker.SlaChecker"
-	shift
-fi
+case "$1" in
+	--std-agent=*)
+		EXTRA_OPTS="-javaagent:out/jar/spl-agent.jar=spl.shutdown.class=cz.cuni.mff.d3s.spl.example.newton.checker.SlaChecker:`echo \"$1\" | cut '-d=' -f 2`"
+		shift
+		;;
+	--std-agent)
+		EXTRA_OPTS="-javaagent:out/jar/spl-agent.jar=spl.shutdown.class=cz.cuni.mff.d3s.spl.example.newton.checker.SlaChecker"
+		shift
+		;;
+	*)
+		;;
+esac
 
 MY_CLASSPATH="out/classes:lib/asm-debug-all-4.0.jar:llib/dislserver-unspec.jar:lib/commons-math-2.2.jar"
 
@@ -29,6 +37,7 @@ randomize_taskset() {
 }
 
 SUM="0"
+DATA_VECTOR="NaN";
 for i in `seq 1 $LOOPS`; do
 	echo "Starting loop $i (out of $LOOPS)"
 	randomize_taskset
@@ -36,7 +45,14 @@ for i in `seq 1 $LOOPS`; do
 	MEASURED=`sed -n 's/.*loops of solving.*[(]\([0-9]*\)ms.*/\1/p' <$$.tmp`
 	rm -f $$.tmp
 	SUM="$SUM+$MEASURED"
+	DATA_VECTOR="$DATA_VECTOR,$MEASURED"
 done
 rm -f $$.tmp
 AVG=`echo '(' "$SUM" ')/' "$LOOPS" | bc`
-echo ">>>>> Average run-time is ${AVG}ms."
+STATS=`cat <<R_SCRIPT | Rscript --vanilla /dev/stdin
+x <- c($DATA_VECTOR)
+x.mean <- mean(x, na.rm=TRUE)
+x.sd <- sd(x, na.rm=TRUE)
+writeLines(sprintf(fmt="Mean is %0.1fms (std.dev is %0.2f)", x.mean, x.sd), sep="")
+R_SCRIPT`
+echo ">>>>> $STATS."
