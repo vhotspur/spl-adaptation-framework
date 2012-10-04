2fa7bd40ce2d8554c540864805ca8a07fe54976d
Vojtech Horky <vojtech.horky@gmail.com>
2012-10-04 08:20:44 +0200

    Add dump function to ArtificialSerieDataSource

--- src/cz/cuni/mff/d3s/spl/core/data/artificial/ArtificialSerieDataSource.java
+++ src/cz/cuni/mff/d3s/spl/core/data/artificial/ArtificialSerieDataSource.java
@@ -1,5 +1,8 @@
 package cz.cuni.mff.d3s.spl.core.data.artificial;
 
+import java.util.Map;
+import java.util.SortedMap;
+
 import cz.cuni.mff.d3s.spl.agent.Access;
 import cz.cuni.mff.d3s.spl.core.data.SampleStorage;
 import cz.cuni.mff.d3s.spl.core.data.SerieDataSource;
@@ -18,5 +21,19 @@ public class ArtificialSerieDataSource extends OriginalSerieDataSource {
 	
 	public void addSample(long sampleNanos, long clockMillis) {
 		getStorage().add(sampleNanos, clockMillis);
+	}
+	
+	public String dumpAllSamples() {
+		SortedMap<Long, Long> allSamples = getStorage().get(Long.MIN_VALUE, Long.MAX_VALUE);
+		StringBuilder result = new StringBuilder();
+		for (Map.Entry<Long, Long> sample : allSamples.entrySet()) {
+			result.append(formatSample(sample.getKey(), sample.getValue()));
+			result.append(" ");
+		}
+		return result.toString().trim();
+	}
+	
+	private String formatSample(long clock, long value) {
+		return String.format("(%d, %d)", clock, value);
 	}
 }
