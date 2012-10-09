d60c72a9297068980593fda08b000382686e3b8f
Vojtech Horky <vojtech.horky@gmail.com>
2012-10-09 09:34:04 +0200

    Add SlidingSlotDataSource (with a test)

---
+++ src-test/cz/cuni/mff/d3s/spl/core/data/segment/SlidingSlotDataSourceTest.java
@@ -0,0 +1,45 @@
+package cz.cuni.mff.d3s.spl.core.data.segment;
+
+import org.junit.Test;
+
+import cz.cuni.mff.d3s.spl.core.data.DataSource;
+import cz.cuni.mff.d3s.spl.core.data.PrecomputedStatistics;
+import cz.cuni.mff.d3s.spl.core.data.Statistics;
+import cz.cuni.mff.d3s.spl.test.TestUtils;
+
+public class SlidingSlotDataSourceTest {
+
+	private final static double EPSILON = 0.0001;
+	private final Statistics emptyStatistics
+		= PrecomputedStatistics.create(Double.NaN, 0);
+	
+	private void assertSourceHasEmptyStatistics(DataSource source) {
+		TestUtils.assertStatisticsEqual(source.get(), emptyStatistics, EPSILON);
+	}
+	
+	@Test
+	public void emptySourceProducesEmptyStatistics() {
+		SlidingSlotDataSource empty = new SlidingSlotDataSource(0, 10);
+		assertSourceHasEmptyStatistics(empty);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void invalidInternalThrowsException() {
+		new SlidingSlotDataSource(10, 9);
+	}
+	
+	@Test
+	public void emptyIntervalSourceRemainsEmpty() {
+		SlidingSlotDataSource source = new SlidingSlotDataSource(10, 10);
+		assertSourceHasEmptyStatistics(source);
+		
+		source.newSample(10, 5);
+		assertSourceHasEmptyStatistics(source);
+		
+		source.newSample(20, 10);
+		assertSourceHasEmptyStatistics(source);
+		
+		source.newSample(30, 15);
+		assertSourceHasEmptyStatistics(source);
+	}
+}
---
+++ src/cz/cuni/mff/d3s/spl/core/data/segment/SlidingSlotDataSource.java
@@ -0,0 +1,43 @@
+package cz.cuni.mff.d3s.spl.core.data.segment;
+
+import cz.cuni.mff.d3s.spl.core.data.PrecomputedStatistics;
+import cz.cuni.mff.d3s.spl.core.data.SampleBasedDataSource;
+import cz.cuni.mff.d3s.spl.core.data.Statistics;
+
+public class SlidingSlotDataSource implements SampleBasedDataSource {
+
+	private long clockStart = Long.MIN_VALUE;
+	private long clockEnd = Long.MAX_VALUE;
+	private long sampleCount = 0;
+	private double sampleSum = 0.0;
+	
+	
+	public SlidingSlotDataSource(long start, long end) {
+		clockStart = start;
+		clockEnd = end;
+	}
+
+	@Override
+	public Statistics get() {
+		if (sampleCount == 0) {
+			return PrecomputedStatistics.create(Double.NaN, 0);
+		}
+		return PrecomputedStatistics.create(sampleSum / (double) sampleCount, sampleCount);
+	}
+
+	@Override
+	public void newSample(long sample, long clock) {
+		if ((clock < clockStart) || (clock >= clockEnd)) {
+			return;
+		}
+		
+		sampleCount++;
+		sampleSum += (double) sample;
+	}
+
+	@Override
+	public void addSubSource(SampleBasedDataSource subSource) {
+		throw new UnsupportedOperationException("Not implemented yet.");
+	}
+
+}
