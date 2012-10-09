60747e242b094a0a8b26b733cf7a1f430def7721
Vojtech Horky <vojtech.horky@gmail.com>
2012-10-09 08:31:00 +0200

    Dummy data source implements SampleBasedDataSource

--- src/cz/cuni/mff/d3s/spl/core/data/artificial/DummySerieDataSource.java
+++ src/cz/cuni/mff/d3s/spl/core/data/artificial/DummySerieDataSource.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 
 import cz.cuni.mff.d3s.spl.core.data.PrecomputedStatistics;
+import cz.cuni.mff.d3s.spl.core.data.SampleBasedDataSource;
 import cz.cuni.mff.d3s.spl.core.data.SerieDataSource;
 import cz.cuni.mff.d3s.spl.core.data.Statistics;
 
-public class DummySerieDataSource implements SerieDataSource {
+public class DummySerieDataSource implements SerieDataSource, SampleBasedDataSource {
 	
 	private Statistics stats;
 	private static final Long zero = (long) 0;
@@ -25,5 +26,15 @@ public class DummySerieDataSource implements SerieDataSource {
 	@Override
 	public SerieDataSource getSegment(long startTime, long endTime) {
 		return this;
+	}
+
+	@Override
+	public void newSample(long sample, long clock) {
+		/* Ignore. */
+	}
+
+	@Override
+	public void addSubSource(SampleBasedDataSource subSource) {
+		/* Ignore. */
 	}
 }
