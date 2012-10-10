5088f4a1049a464ae25dc427c6920b92cd2bfe86
Vojtech Horky <vojtech.horky@gmail.com>
2012-10-10 11:41:01 +0200

    InstrumentationTest tests sample based source as well

--- src-test/cz/cuni/mff/d3s/spl/test/InstrumentationTest.java
+++ src-test/cz/cuni/mff/d3s/spl/test/InstrumentationTest.java
@@ -5,9 +5,11 @@ import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import cz.cuni.mff.d3s.spl.agent.Access;
 import cz.cuni.mff.d3s.spl.agent.Settings;
 import cz.cuni.mff.d3s.spl.core.data.SerieDataSource;
 import cz.cuni.mff.d3s.spl.core.data.instrumentation.InstrumentingDataSource;
+import cz.cuni.mff.d3s.spl.core.data.storage.CompleteDataSource;
 
 public class InstrumentationTest {
 
@@ -15,22 +17,28 @@ public class InstrumentationTest {
 	private static final String INSTRUMENTED_METHOD = "action";
 	
 	private static SerieDataSource instrumentationSource;
+	private static CompleteDataSource source;
 	
 	@BeforeClass
 	public static void instrumentAction() {
 		Settings.DEFAULT_SKIP_FACTOR = 0;
 		instrumentationSource = InstrumentingDataSource.create(INSTRUMENTED_CLASS, INSTRUMENTED_METHOD);
+		source = CompleteDataSource.createFromMeasurementPoint(Access.getMeasurementPoint(InstrumentingDataSource.createId(INSTRUMENTED_CLASS, INSTRUMENTED_METHOD)));
+		
+		runInstrumentedMethod(1234);
 	}
 	
 	@Test
 	public void allRunsAreRecordedCorrectly() {
-		runInstrumentedMethod(1234);
-		
-		long sampleCount = instrumentationSource.get().getSampleCount();
-		assertEquals(1234, sampleCount);
+		assertEquals(1234, instrumentationSource.get().getSampleCount());
 	}
 	
-	private void runInstrumentedMethod(int count) {
+	@Test
+	public void completeDataSourceRegisteredAllRuns() {
+		assertEquals(1234, source.get().getSampleCount());
+	}
+	
+	private static void runInstrumentedMethod(int count) {
 		while (count > 0) {
 			TestClass.action();
 			count--;
